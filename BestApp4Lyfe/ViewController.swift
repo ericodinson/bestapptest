c449e9f62dda6894b52572c1ebe7d1b502faf34a
Eric Aichele <ericaichele@Erics-MacBook-Pro.local>
2015-06-09 19:34:28 -0400

    Initial Commit

---
+++ BestApp4Lyfe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BestApp4Lyfe
+//
+//  Created by Eric Aichele on 6/9/15.
+//  Copyright (c) 2015 Eric Aichele. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
